ab026cdab0c2914f3c55df54b43ddff695f95ba7
burtonov13 <157344307+burtonov13@users.noreply.github.com>
2024-01-22 15:06:42 +0800

    Add files via upload

---
+++ Урок 2. Ввод и вывод данных/task3.py
@@ -0,0 +1,6 @@
+# Задание №3
+print (1)
+print (2)
+print (3)
+print (4)
+print (5)
---
+++ Урок 2. Ввод и вывод данных/task6.py
@@ -0,0 +1,6 @@
+str_1 = input ('Hello')
+str_2 = input ('World')
+str_3 = input ('!')
+print (str_3)
+print (str_2)
+print (str_1)
